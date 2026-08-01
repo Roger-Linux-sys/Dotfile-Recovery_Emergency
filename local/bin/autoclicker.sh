f6dd1b67742ab91e64ce08c2a535e9e8d2d4ca24
Roger <roger.almeida@aluno.ce.gov.br>
2026-07-31 22:24:30 -0300

    feat: autoclicker now holds button instead of clicking

--- local/bin/autoclicker.sh
+++ local/bin/autoclicker.sh
@@ -2,16 +2,17 @@
 PIDFILE="/tmp/autoclicker.pid"
 
 if [ -f "$PIDFILE" ] && kill -0 $(cat "$PIDFILE") 2>/dev/null; then
+    ydotool mouseup 0xC0
     kill $(cat "$PIDFILE")
     rm -f "$PIDFILE"
-    notify-send -u low -a Autoclicker "Stopped"
+    notify-send -u low -a Autoclicker "Released"
 else
     (
+    ydotool mousedown 0xC0
     while true; do
-        ydotool click 0xC0
-        sleep 0.02
+        sleep 1
     done
     ) &
     echo $! > "$PIDFILE"
-    notify-send -u low -a Autoclicker "Started (50 CPS)"
+    notify-send -u low -a Autoclicker "Holding (toggle off to release)"
 fi
